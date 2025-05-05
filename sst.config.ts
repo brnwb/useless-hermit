b07e6768256155534d91c2b77beece92245351bd
Brian Webb <brian.webbito@gmail.com>
2025-05-05 19:54:26 -0400

    Add SST with Cloudflare DNS

---
+++ sst.config.ts
@@ -0,0 +1,30 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+export default $config({
+  app(input) {
+    return {
+      name: "useless-hermit",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+      providers: {
+        aws: {
+          profile:
+            input.stage === "production"
+              ? "uselesshermit-production"
+              : "uselesshermit-dev",
+          region: "us-east-2",
+        },
+        cloudflare: "6.1.2",
+      },
+    };
+  },
+  async run() {
+    new sst.aws.Astro("MyWeb", {
+      domain: {
+        name: "uselesshermit.com",
+        dns: sst.cloudflare.dns(),
+        redirects: ["www.uselesshermit.com"],
+      },
+    });
+  },
+});
